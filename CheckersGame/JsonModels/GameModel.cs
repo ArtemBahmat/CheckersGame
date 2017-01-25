5aef0570d262ea03af598fc58825061414524736
ArtemBahmat <artem.bahmat@yandex.ru>
2017-01-25 12:12:32 +0200

    Init

---
+++ CheckersGame/JsonModels/GameModel.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CheckersGame.Models;
+
+namespace CheckersGame.JsonModels
+{
+    [Serializable]
+    public class GameModel
+    {
+        private const string SAVE_FILE = "game.json";
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public List<CellModel> Cells { get; set; }
+
+        public GameModel()
+        { }
+        
+        public GameModel(Game game)
+        {
+            Player1 = game.Player1;
+            Player2 = game.Player2;
+            Cells = GetCurrentCells();
+        }
+
+        private List<CellModel> GetCurrentCells()
+        {
+            var cells = GameManager.GetAllCells();
+            List<CellModel> result = new List<CellModel>();
+
+            foreach (var cell in cells)
+            {
+                result.Add(new CellModel(cell.CellPosition, cell.Owner, cell.Disabled));
+            }
+
+            return result;
+        }
+
+        public bool SerializeToFile()
+        {
+            bool success = false;
+
+            try
+            {
+                using (StreamWriter file = File.CreateText(SAVE_FILE))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, this);
+                    success = true;
+                }
+            }
+            catch (Exception)
+            { }
+
+            return success && File.Exists(SAVE_FILE);  
+        }
+
+        public void DeserializeFromFile()
+        {
+            GameModel gameModel;
+
+            try
+            {
+                using (StreamReader file = File.OpenText(SAVE_FILE))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    gameModel = (GameModel)serializer.Deserialize(file, typeof(GameModel));
+                }
+
+                Player1 = gameModel.Player1;
+                Player2 = gameModel.Player2;
+                Cells = gameModel.Cells;
+            }
+            catch (Exception)
+            { }         
+        }
+    }
+}
---
+++ CheckersGame/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using CheckersGame.Models;
+using CheckersGame.JsonModels;
+using System.Windows;
+
+namespace CheckersGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string player1, string player2)
+        {
+            InitializeComponent();
+            GameManager.InitializeGame(this, player1, player2);
+        }
+
+        private void SaveMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = GameManager.Game;
+
+            if (!game.GameOver)
+            {
+                GameModel model = new GameModel(game);
+                string msg = model.SerializeToFile() ? "Successfully saved" : "Error occured while saving";
+                MessageBox.Show(msg);            
+            }       
+        }
+
+        private void LoadMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            GameModel model = new GameModel();
+            model.DeserializeFromFile();
+            GameManager.Game.LoadNewGame(model);
+        }
+
+        private void RestartItem_Click(object sender, RoutedEventArgs e)
+        {
+            GameManager.Game.RestartGame();
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
